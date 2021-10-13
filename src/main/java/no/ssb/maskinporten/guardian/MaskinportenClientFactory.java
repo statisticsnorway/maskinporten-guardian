29d5002239b49875811e9ebb9a2a06cc3a62f6bd
Rupinder Kaur <Rupinder.Kaur@ssb.no>
2021-10-13 08:47:37 +0200

    Add test with refactoring (#6)
    * Added test case to mock external dependency
    
    * Added mock test for AccessTokenController
    
    * Refactoring changes: removed service class and added client in controller
    
    * Mocked MeterRegistry dependency
    
    * Added assert in the test
    
    * Removed service class. Integrated MaskinportenCLient in controller
    
    * assert http ok and not null body
    
    * return new principal with Lambda
    
    * fix typo in test method name
    
    * Corrected type: MaskinPorten->Maskinporten
    
    Co-authored-by: Olve Drageset <olve.drageset@gmail.no>

--- src/main/java/no/ssb/maskinporten/guardian/MaskinportenClientFactory.java
+++ src/main/java/no/ssb/maskinporten/guardian/MaskinportenClientFactory.java
@@ -8,46 +8,22 @@ import no.ks.fiks.maskinporten.MaskinportenklientProperties;
 import no.ssb.maskinporten.guardian.config.CertificateConfig;
 import no.ssb.maskinporten.guardian.config.MaskinportenClientConfig;
 import no.ssb.maskinporten.guardian.config.MaskinportenConfig;
-import no.ssb.maskinporten.guardian.secret.SecretService;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
 @Singleton
 @RequiredArgsConstructor
 public class MaskinportenClientFactory {
 
-    private final SecretService secretService;
-    private final CertificateConfig certificateConfig;
     private final MaskinportenConfig maskinportenConfig;
-
-    private KeyStore loadKeyStore(char[] keyStorePassword) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-        KeyStore keyStore = KeyStore.getInstance("pkcs12");
-        keyStore.load(fetchKeyStore(), keyStorePassword);
-        return keyStore;
-    }
-
-    InputStream fetchKeyStore() {
-        byte[] cert = secretService.getSecret(certificateConfig.getCertificateSecretId());
-        return new ByteArrayInputStream(cert);
-    }
-
-    char[] fetchKeyStorePassword() {
-        byte[] keyStorePassword = secretService.getSecret(certificateConfig.getCertificatePassphraseSecretId());
-        return new String(keyStorePassword, StandardCharsets.UTF_8).toCharArray();
-    }
+    private final MaskinportenKeyStore maskinPortenKeyStore;
+    private final CertificateConfig certificateConfig;
 
     // TODO: Keep client instances in a timed cache?
     @SneakyThrows
     public Maskinportenklient maskinportenClient(String clientId) {
-        char[] keyStorePassword = fetchKeyStorePassword();
-        KeyStore keyStore = loadKeyStore(keyStorePassword);
+        char[] keyStorePassword = maskinPortenKeyStore.fetchKeyStorePassword();
+        KeyStore keyStore = maskinPortenKeyStore.loadKeyStore(keyStorePassword);
 
         MaskinportenClientConfig clientConfig = maskinportenConfig.getClientConfig(clientId);
         Maskinportenklient maskinporten = new Maskinportenklient(keyStore, certificateConfig.getCertificateKeystoreEntryAlias(), keyStorePassword, MaskinportenklientProperties.builder()
