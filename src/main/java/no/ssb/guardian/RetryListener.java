352085aecd48c64db648ad4833ec2e1eed5c0406
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-04-25 15:10:02 +0200

    Improve logging for RetryListener

--- src/main/java/no/ssb/guardian/RetryListener.java
+++ src/main/java/no/ssb/guardian/RetryListener.java
@@ -5,16 +5,28 @@ import io.micronaut.retry.event.RetryEvent;
 import io.micronaut.retry.event.RetryEventListener;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import no.ssb.guardian.core.util.PrincipalUtil;
+
+import java.security.Principal;
 
 @Slf4j
 @Singleton
 public class RetryListener implements RetryEventListener {
     @Override
     public void onApplicationEvent(RetryEvent event) {
+        final MutableArgumentValue<?> principal = event.getSource().getParameters().get("principal");
         final MutableArgumentValue<?> request = event.getSource().getParameters().get("request");
-        log.error("Request failed {} time(s) for {}:", event.getRetryState().currentAttempt(),
-                request != null ? request.getValue(): "<empty request>",
-                event.getThrowable());
+        if (event.getRetryState().currentAttempt() < 3) {
+            log.warn("Request failed {} time(s) for {} and principal {}:", event.getRetryState().currentAttempt(),
+                    request != null ? request.getValue(): "<empty request>",
+                    principal != null ? PrincipalUtil.auditInfoOf((Principal) principal.getValue()): "<empty principal>",
+                    event.getThrowable());
+        } else {
+            log.error("Request failed {} time(s) for {} and principal {}:", event.getRetryState().currentAttempt(),
+                    request != null ? request.getValue(): "<empty request>",
+                    principal != null ? PrincipalUtil.auditInfoOf((Principal) principal.getValue()): "<empty principal>",
+                    event.getThrowable());
+        }
     }
 
     @Override
