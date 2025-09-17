88cb0d1137b091b04f88e554a016077452df9a03
rupinder <rupinder.kaur@ssb.no>
2025-09-17 14:27:00 +0200

    Update maskinportenServiceAccountAccessToken test to use new scopes

--- src/test/java/no/ssb/guardian/testsupport/KeycloakDevTokenIssuerTest.java
+++ src/test/java/no/ssb/guardian/testsupport/KeycloakDevTokenIssuerTest.java
@@ -9,8 +9,7 @@ public class KeycloakDevTokenIssuerTest {
     @Test
     void maskinportenServiceAccountAccessToken() {
         String maskinportenClientId = "7ea43b76-6b7d-49e8-af2b-4114ebb66c80";
-        String maskinportenServiceAccountAccessToken = KeycloakDevTokenIssuer.maskinportenServiceAccountAccessToken(maskinportenClientId, List.of("kartverk:matrikkel.berettigetinteresse"));
-        System.out.println(maskinportenServiceAccountAccessToken);
+        KeycloakDevTokenIssuer.maskinportenServiceAccountAccessToken(maskinportenClientId, List.of("acme:scope1", "acme:scope2"));
     }
 
     @Test
