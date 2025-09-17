6ea64d20ec64cef1568d2ef624f3224263e6d5a6
rupinder <rupinder.kaur@ssb.no>
2025-09-17 14:25:22 +0200

    Add support for Skyporten audience in token claims

--- src/test/java/no/ssb/guardian/testsupport/KeycloakDevTokenIssuerTest.java
+++ src/test/java/no/ssb/guardian/testsupport/KeycloakDevTokenIssuerTest.java
@@ -9,7 +9,8 @@ public class KeycloakDevTokenIssuerTest {
     @Test
     void maskinportenServiceAccountAccessToken() {
         String maskinportenClientId = "7ea43b76-6b7d-49e8-af2b-4114ebb66c80";
-        KeycloakDevTokenIssuer.maskinportenServiceAccountAccessToken(maskinportenClientId, List.of("acme:scope1", "acme:scope2"));
+        String maskinportenServiceAccountAccessToken = KeycloakDevTokenIssuer.maskinportenServiceAccountAccessToken(maskinportenClientId, List.of("kartverk:matrikkel.berettigetinteresse"));
+        System.out.println(maskinportenServiceAccountAccessToken);
     }
 
     @Test
