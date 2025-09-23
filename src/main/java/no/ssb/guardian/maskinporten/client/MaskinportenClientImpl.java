0b1350bb0e0675f36fefd3de51cb16c1129ff902
rupinder <rupinder.kaur@ssb.no>
2025-09-23 09:03:11 +0200

    Add logging for access token requests in MaskinportenClientImpl

--- src/main/java/no/ssb/guardian/maskinporten/client/MaskinportenClientImpl.java
+++ src/main/java/no/ssb/guardian/maskinporten/client/MaskinportenClientImpl.java
@@ -4,11 +4,15 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import no.ks.fiks.maskinporten.AccessTokenRequest;
 import no.ks.fiks.maskinporten.Maskinportenklient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
 @RequiredArgsConstructor
 public class MaskinportenClientImpl implements MaskinportenClient {
+
+    private static final Logger log = LoggerFactory.getLogger(MaskinportenClientImpl.class);
 
     @NonNull
     private final Maskinportenklient maskinportenklient;
@@ -17,8 +21,11 @@ public class MaskinportenClientImpl implements MaskinportenClient {
      * Retrieve maskinporten access token with specified scopes
      */
     public String getAccessToken(@NonNull Set<String> scopes) {
+        log.info("Requesting access token with scopes: {}", scopes);
         AccessTokenRequest request = AccessTokenRequest.builder().scopes(scopes).build();
-        return maskinportenklient.getAccessToken(request);
+        String token = maskinportenklient.getAccessToken(request);
+        log.debug("Received access token for scopes {}: {}", scopes, token != null ? "[REDACTED]" : "null");
+        return token;
     }
 
      /**
@@ -28,11 +35,14 @@ public class MaskinportenClientImpl implements MaskinportenClient {
      * @return access token that can be used to access APIs protected by maskinporten
      */
     public String getAccessToken(@NonNull Set<String> scopes, String accessTokenAudience) {
+        log.info("Requesting access token with scopes: {} and audience: {}", scopes, accessTokenAudience);
         AccessTokenRequest request = AccessTokenRequest.builder()
                 .scopes(scopes)
                 .audience(accessTokenAudience)
                 .build();
-        return maskinportenklient.getAccessToken(request);
+        String token = maskinportenklient.getAccessToken(request);
+        log.debug("Received access token for scopes {} and audience {}: {}", scopes, accessTokenAudience, token != null ? "[REDACTED]" : "null");
+        return token;
     }
 
 }
